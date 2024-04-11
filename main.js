4adaedc0ec042dfbd618f81e9a3f3728f61eb1c3
EriCrysBel <erikacrystal95@gmail.com>
2024-04-11 13:33:31 +0200

    crear funciones y display base

--- main.js
+++ main.js
@@ -1,35 +1,21 @@
-//Api de localStorage
-//dentro  del metodo setItem entre parentesis se guarda el nombre  y la informacion que queremos guardar en el storage. Esta compuesto por una CLAVE a la izquierda y un VALOR a la derecha
-//Guardar en Local Storage
-// localStorage.setItem('tarea2', 'fiesta loca')
-// let tarea = localStorage.getItem('tarea2')//devuelve el valor que tiene la clave 'tarea01' en el almacenamiento
-
-// const tareas = localStorage
-// console.log(tareas.length)
-// for (let i = 0; i < tareas.length; i++) {
-//     let key = localStorage.key(i) // Clave donde esta guardada la info del Est.
-//     console.log(key)
-//     tarea = localStorage.getItem(key) // Info del Est. en formato JSON
-//     console.log(tarea)
-//   }
-
-// console.log(tarea)
-// console.log(localStorage.length)
-
-// localStorage.clear();
-
-const notaInput = document.getElementById('nota');
-const btnGuardar = document.getElementById('btn-guardar');
-const almacen = document.getElementById('almacen-notas');
-
-btnGuardar.addEventListener('click', () => {
-  const nota = notaInput.value;
-  localStorage.setItem('nota', nota);
-  // Mostrar mensaje de bienvenida
-  almacen.textContent =`${nota}`;
-});
-
-notaGuardada = localStorage.getItem( 'nota' );
-if( notaGuardada != null  ) {
-    almacen.textContent = `${notaGuardada} ` ;
+//recoger la información
+function getInfo(){
+  console.log('texto-ingresado')
+  return 'texto ingresado'
+};
+//almacenar en localstorage
+function setLocalInfo(){
+//Guardar la ingo en localstorage
+return true
+}
+//leerla  y mostrarla por pantalla
+function showInfo(){
+  //leerla y mostrarla por pantalla
+  //si no hay nada mostrar 'no hay informacion almacenada'
+  return true
+}
+//limpiarla
+function cleanInfo(){
+  //limpiar el localstorage
+  return true
 }
